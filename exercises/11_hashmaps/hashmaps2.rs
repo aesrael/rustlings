e91f07b363a12292acdfcb70497bca269d0c800b
Israel Adura <israeladura@gmail.com>
2025-01-28 10:05:49 +0000

    update

--- exercises/11_hashmaps/hashmaps2.rs
+++ exercises/11_hashmaps/hashmaps2.rs
@@ -32,6 +32,11 @@ fn fruit_basket(basket: &mut HashMap<Fruit, u32>) {
         // TODO: Insert new fruits if they are not already present in the
         // basket. Note that you are not allowed to put any type of fruit that's
         // already present!
+        basket.entry(fruit).or_insert(1);
+        // let count = basket.get(&fruit).copied().unwrap_or(0);
+        // if count == 0 {
+        //     basket.insert(fruit, 1);
+        // }
     }
 }
 
